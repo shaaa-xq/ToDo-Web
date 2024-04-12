30392582a0f113472ea8a2f2f3d42dc822843e0e
shaaa-xq <ayeshacha177@gmail.com>
2024-04-12 21:18:47 +0700

    up

--- fitur/task/script.js
+++ fitur/task/script.js
@@ -1,7 +1,24 @@
-// add task
+function addTask() {
+  const arrayTask = [];
+  const titleTask = document.getElementById("t-title");
+  const dateTask = document.getElementById("t-date");
 
-const addTaskButton = document.getElementById("btn");
+  const titleTaskText = titleTask.value;
+  const date = dateTask.value;
 
-addTaskButton.addEventListener("click", function () {
-  alert();
-});
+  arrayTask.push(titleTaskText, date);
+
+  console.log(arrayTask);
+}
+// let list = [
+//   {
+//     title: "tugas a",
+//     date: "27/7/24",
+//     subject: "ui ux",
+//   },
+//   {
+//     title: "tugas b",
+//     date: "8/8/24",
+//     subject: "english",
+//   },
+// ];
