453c222aac33f1c1a7ea7b966b058eb859042e43
shaaa-xq <ayeshacha177@gmail.com>
2024-04-15 20:22:34 +0700

    test

--- fitur/task/script.js
+++ fitur/task/script.js
@@ -8,10 +8,15 @@ function addTask() {
   const display = document.getElementById("display");
   const newTask = document.createElement("li");
 
-  newTask.innerHTML = `${titleText} (Deadline: ${date})`;
+  newTask.innerHTML = `${titleText} (Deadline: ${date}) <button style="padding: 5px; border-radius: 10px" onclick="removeTask(this)">Delete</button>`;
 
   display.appendChild(newTask);
 
   titleTask.value = "";
   dateTask.value = "";
+}
+
+function removeTask(button) {
+  const taskList = button.parentElement;
+  taskList.remove();
 }
