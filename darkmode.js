406da479e9277e157f24bdedeff6e57c58ed9324
shaaa-xq <ayeshacha177@gmail.com>
2024-04-29 16:33:27 +0700

    light mode toggle

---
+++ darkmode.js
@@ -0,0 +1,11 @@
+const darkToggle = document.getElementById("toggle");
+
+darkToggle.addEventListener("click", function () {
+  const body = document.getElementsByTagName("body")[0];
+  body.classList.toggle("dark-mode");
+  body.classList.toggle("light-mode");
+  body.classList.toggle("light-welcome");
+  body.classList.toggle("light-datetime");
+  body.classList.toggle("dark-welcome");
+  body.classList.toggle("dark-datetime");
+});
